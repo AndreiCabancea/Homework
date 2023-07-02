fc9519217486635e60c0c16ffe86949b96dcae0b
andrei cabancea <133905381+AndreiCabancea@users.noreply.github.com>
2023-07-02 14:22:22 +0300

    Add files via upload

---
+++ RelationOperators.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class RelationOperators {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 50;
+
+
+        System.out.println("a=" + a + ", w="+ b);
+        System.out.println("a > b: " + (a <b));
+        System.out.println("a < b: " + (a >b));
+
+        System.out.println("a >= b: " + (a <= b));
+        System.out.println("a <= b: " + (a >= b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a !=b));
+
+
+
+    }
+}
